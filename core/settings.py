d800d253771f971c4c5557038ba9c58871eda728
Hoàng Văn Giỏi <gioitube2k2@gmail.com>
2023-07-18 11:16:47 +0700

    add static dir

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', default='127.0.0.1')]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(' ') 
 
 
 # Application definition
