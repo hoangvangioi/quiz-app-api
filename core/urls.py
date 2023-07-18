dd53bc2997c8d18a820ac75a8f1005100c620600
Hoàng Văn Giỏi <gioitube2k2@gmail.com>
2023-07-18 09:39:20 +0700

    add favicon.ico

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.generic.base import RedirectView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -34,6 +36,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^favicon\.ico$', RedirectView.as_view(url='../static/favicon.svg')),
 
     # swagger
     path('swagger/', schema_view.with_ui('swagger',
@@ -47,3 +50,7 @@ urlpatterns = [
     # api
     path('api/', include('api.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
